5da3688c84f797713a78cd82ff3f0796b249a57d
manku19978 <55312284+manku19978@users.noreply.github.com>
2022-08-26 17:46:07 +0530

    drag and drop between list

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState, useRef } from "react";
+
+const App = () => {
+  const startIndex = useRef(null);
+  const [list, setList] = useState([1,2,3,4,5]);
+
+  const handleDragStart = (i) => {
+    startIndex.current = i;
+  }
+
+  const handleDragOver = (e) => {
+    e.preventDefault()
+  }
+
+  const handleDrop = (i) => {
+    const temp = list;
+    const startEle = temp[startIndex.current]
+    temp.splice(startIndex.current, 1)
+    temp.splice(i, 0, startEle)
+    setList(temp)
+  }
+
+  const listToRender = list.map((e, i) => {
+    return (
+      <div 
+        className="tile" 
+        key={i}
+        draggable
+        onDragStart={() => handleDragStart(i)}
+        onDragOver={handleDragOver}
+        onDrop={() => handleDrop(i)}
+      >
+        {e}
+      </div>
+    )
+  })
+
+  return (
+    <div className="App">
+      {listToRender}
+    </div>
+  );
+}
+
+export default App;
